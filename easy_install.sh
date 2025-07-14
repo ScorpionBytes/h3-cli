3881830ab6feb20b5a4094a3bb96394ccd1dc0a3
cody.edwards <cody.edwards@horizon3.ai>
2025-07-14 14:29:13 +0000

    NG upgrade

--- easy_install.sh
+++ easy_install.sh
@@ -3,7 +3,7 @@
 # deps: git, unzip or tar
 #
 # This script can be downloaded and executed using a single command:
-#   $ curl https://raw.githubusercontent.com/horizon3ai/h3-cli/public/easy_install.sh | bash -s [ {api-key} [ {runner-name} [ {h3-env} ] ] ]
+#   $ curl https://raw.githubusercontent.com/horizon3ai/h3-cli/public/easy_install.sh | bash -s [ {api-key} [ {runner-name} [ {h3-env} [ {h3-download-url} ] ] ] ]
 #
 # The script downloads h3-cli and runs the install script (install.sh), passing in the {api-key}, if provided.
 #
@@ -21,11 +21,11 @@
 #
 
 
-function echoerr { 
+function echoerr {
     echo "[`date`] $@" 1>&2;   
 }
 
-function echolog { 
+function echolog {
     if [ "$H3_CLI_VERBOSE" = "1" ]; then
         echo "[`date`] $@" 1>&2;   
     fi
@@ -36,27 +36,12 @@ function via_git_pull {
     if ! command -v git &> /dev/null; then
         return 1
     fi
-    if [ ! -d "$H3_CLI_HOME/.git" ]; then 
+    if [ ! -d "$H3_CLI_HOME/.git" ]; then
         return 1
     fi
     echoerr "INFO: $H3_CLI_HOME/.git detected; upgrading via git pull"
     cd $H3_CLI_HOME
     git pull
-}
-
-# :returns: download dir
-function via_git_clone {
-    if ! command -v git &> /dev/null; then
-        return 1
-    fi
-    echoerr "INFO: Downloading via git "
-    git clone https://github.com/horizon3ai/h3-cli.git
-    rc=$?
-    if [ $rc -ne 0 ]; then 
-        echoerr "ERROR: git clone failed"
-        exit 1
-    fi
-    echo "`pwd`/h3-cli"
 }
 
 # :returns: download dir
@@ -68,13 +53,13 @@ function via_unzip {
     zip_url=https://github.com/horizon3ai/h3-cli/archive/refs/heads/public.zip
     curl -sL $zip_url -o h3-cli-public.zip
     rc=$?
-    if [ $rc -ne 0 ]; then 
+    if [ $rc -ne 0 ]; then
         echoerr "ERROR: curl $zip_url failed"
         exit 1
     fi
-    unzip -qo h3-cli-public.zip 
+    unzip -qo h3-cli-public.zip
     rc=$?
-    if [ $rc -ne 0 ]; then 
+    if [ $rc -ne 0 ]; then
         echoerr "ERROR: unzip h3-cli-public.zip failed"
         exit 1
     fi
@@ -87,66 +72,145 @@ function via_tar {
         return 1
     fi
     echoerr "INFO: Downloading via curl + tar"
-    tar_url=https://github.com/horizon3ai/h3-cli/archive/refs/heads/public.tar.gz 
+    tar_url=https://github.com/horizon3ai/h3-cli/archive/refs/heads/public.tar.gz
     curl -sL $tar_url | tar -zx
     rc=$?
-    if [ $rc -ne 0 ]; then 
+    if [ $rc -ne 0 ]; then
         echoerr "ERROR: curl $tar_url | tar failed"
         exit 1
     fi
     echo "`pwd`/h3-cli-public"
 }
 
+# :returns: download dir
+function via_unzip_ng {
+    # Set default download URL
+    if ! command -v unzip &> /dev/null; then
+        echoerr "ERROR: h3-cli requires unzip to download."
+        return 1
+    fi
+    echoerr "INFO: Downloading via curl + unzip"
+    zip_url="$H3_CLI_DOWNLOAD_URL"
+    curl -sL $zip_url -o h3-cli.zip
+    rc=$?
+    if [ $rc -ne 0 ]; then
+        echoerr "ERROR: curl $zip_url failed"
+        exit 1
+    fi
+    unzip -qo h3-cli.zip -d h3-cli
+    rc=$?
+    if [ $rc -ne 0 ]; then
+        echoerr "ERROR: unzip h3-cli.zip failed"
+        exit 1
+    fi
+    echo "`pwd`/h3-cli"
+}
+
 # downloads into $H3_CLI_HOME
 function download_h3_cli {
     echoerr "INFO: Downloading h3-cli into $H3_CLI_HOME ..."
-    via_git_pull
-    rc=$?
-    if [ $rc -eq 0 ]; then 
-        return 0
-    fi
-    # download into tmp dir first, to handle diffs between download methods,
-    # eg. how git creates h3-cli dir and the zip download creates h3-cli-public dir.
-    install_tmp_basedir="`pwd`/.h3-cli-install-tmp"
-    mkdir -p "$install_tmp_basedir"
-    cd "$install_tmp_basedir"
-    tmp_d=`via_git_clone`
-    rc=$?
-    if [ $rc -ne 0 ]; then 
+
+    if [ -n "$H3_CLI_DOWNLOAD_URL" ]; then
+        echoerr "INFO: Attempting to download h3-cli via NodeZero Gateway (NG) into $H3_CLI_HOME ..."
+
+        install_tmp_basedir="`pwd`/.h3-cli-install-tmp"
+        mkdir -p "$install_tmp_basedir"
+        cd "$install_tmp_basedir"
+        tmp_d=`via_unzip_ng`
+        rc=$?
+        if [ $rc -ne 0 ]; then
+            echoerr "ERROR: Failed to download h3-cli. Please check that your network has a connection to $H3_CLI_DOWNLOAD_URL, and try again."
+            exit 1
+        fi
+
+    else
+        echoerr "INFO: Attempting to download h3-cli via git into $H3_CLI_HOME ..."
+        via_git_pull
+        rc=$?
+        if [ $rc -eq 0 ]; then
+            return 0
+        fi
+        # download into tmp dir first, to handle diffs between download methods,
+        # eg. how git creates h3-cli dir and the zip download creates h3-cli-public dir.
+        install_tmp_basedir="`pwd`/.h3-cli-install-tmp"
+        mkdir -p "$install_tmp_basedir"
+        cd "$install_tmp_basedir"
+        echoerr "INFO: Attempting to downloading h3-cli via GitHub into $H3_CLI_HOME ..."
         tmp_d=`via_unzip`
         rc=$?
-        if [ $rc -ne 0 ]; then 
+        if [ $rc -ne 0 ]; then
             tmp_d=`via_tar`
             rc=$?
-            if [ $rc -ne 0 ]; then 
+            if [ $rc -ne 0 ]; then
                 echoerr "ERROR: Failed to download h3-cli repo"
                 exit 1
             fi
         fi
     fi
-    # move into the target dir 
+
+    # move into the target dir
     cp -R "$tmp_d/." "$H3_CLI_HOME"
     # clean up tmp dir
     rm -Rf "$install_tmp_basedir"
 }
 
-
 echoerr "INFO: Installing h3-cli ..."
 api_key=$1
 runner_name=$2
 h3_env=$3
+h3_download_url=$4
 
 # 0.
 # check deps
-if ! command -v git &> /dev/null; then
-    if ! command -v unzip &> /dev/null; then
-        if ! command -v tar &> /dev/null; then
-            echoerr "ERROR: h3-cli requires git, unzip or tar to download."
-            exit 1
-        fi
+if [ -z "$HOME" -o ! -e "$HOME" ]; then 
+    echoerr "ERROR: This script requires the HOME environent variable to be set to the user's home directory."
+    exit 1
+fi
+
+
+config_file="$HOME/.h3/__global__.env"
+# Determine if we should use NodeZero Gateway (NG) or GitHub for downloading
+# config_file does not exist, create and write the setting if h3_download_url is provided
+if [ ! -e "$config_file" ]; then
+
+    # Write h3_download_url to config file
+    if [ -n "$h3_download_url" ]; then
+        H3_CLI_DOWNLOAD_URL=$h3_download_url
+        echo "H3_CLI_DOWNLOAD_URL=$H3_CLI_DOWNLOAD_URL" > "$config_file"
+    fi
+    # Dont create the file if h3_download_url is not provided
+
+# config_file exists, source the file
+else
+    source "$config_file"
+
+    # h3_download_url is provided, overwrite the setting and set it in the config file
+    if [ -n "$h3_download_url" ]; then
+        H3_CLI_DOWNLOAD_URL=$h3_download_url
+        echo "H3_CLI_DOWNLOAD_URL=$H3_CLI_DOWNLOAD_URL" > "$config_file"
     fi
 fi
 
+# Library checks for using NodeZero Gateway (NG) for h3-cli installation
+if [ -n "$H3_CLI_DOWNLOAD_URL" ]; then
+
+    # Only .zip is supported for the NG
+    if ! command -v unzip &> /dev/null; then
+        echoerr "ERROR: h3-cli requires unzip to download."
+        exit 1
+    fi
+
+# Library checks for using GitHub for h3-cli installation
+else
+    if ! command -v git &> /dev/null; then
+        if ! command -v unzip &> /dev/null; then
+            if ! command -v tar &> /dev/null; then
+                echoerr "ERROR: h3-cli requires git, unzip or tar to download."
+                exit 1
+            fi
+        fi
+    fi
+fi
 
 # 1.
 # determine H3_CLI_HOME, where h3-cli will be downloaded/upgraded.
